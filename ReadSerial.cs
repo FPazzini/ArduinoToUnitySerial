cccd983f1dd08cdc3049c33da3500247601bcf20
Fabio Pazzini <fabio.pazzini@icloud.com>
2018-12-16 16:51:20 +0100

    Perfect communication Arduino to Unity via Serial Port. Just need to refine a couple of things, such as covering the case in which Unity reads only a substring of the whole string sent in from the Arduino.

---
+++ ReadSerial.cs
@@ -0,0 +1,33 @@
+﻿using System.IO.Ports;
+using UnityEngine;
+using System.Collections;
+using System;
+
+public class ReadSerial : MonoBehaviour {
+    SerialPort serialPort;
+	// Use this for initialization
+	void Start () {
+        try {
+            serialPort = new SerialPort("COM3", 115200);
+            if (!serialPort.IsOpen) {
+                serialPort.Open();
+            }
+            serialPort.ReadTimeout = 1;
+        } catch (Exception e) {
+            Debug.Log(e.ToString());
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        try {
+            Debug.Log(serialPort.ReadLine());
+        } catch (Exception e) {
+            Debug.Log(e.ToString());
+        }
+	}
+
+    IEnumerator openPort() {
+        yield return new WaitForSeconds(3);
+    }
+}
